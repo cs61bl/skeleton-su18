5538e5992e0c18b1d435c53281553444035d1134
Kevin Lin <kevinlin1@berkeley.edu>
2018-06-28 18:41:15 -0800

    lab08: Add lab08

---
+++ lab08/BooleanSetTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BooleanSetTest {
+
+    @Test
+    public void testConstructor() {
+        BooleanSet aSet = new BooleanSet(100);
+        SimpleSet anotherSet = aSet;
+        SimpleCollection anotherCollection = aSet;
+    }
+
+    @Test
+    public void testBasics() {
+        BooleanSet aSet = new BooleanSet(100);
+        assertEquals(0, aSet.size());
+        for (int i = 0; i < 100; i += 2) {
+            aSet.add(i);
+            assertTrue(aSet.contains(i));
+        }
+        assertEquals(50, aSet.size());
+
+        for (int i = 0; i < 100; i += 2) {
+            aSet.remove(i);
+            assertFalse(aSet.contains(i));
+        }
+        assertTrue(aSet.isEmpty());
+        assertEquals(0, aSet.size());
+    }
+}
